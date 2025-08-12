beea0261c980dc3d2dde0e664535ccd9cdfc3343
Romain Libaert <git@rlibaert.dev>
2025-08-13 01:05:21 +0200

    feat: add logWriter

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/VictoriaMetrics/metrics"
@@ -22,18 +24,20 @@ type Options struct {
 
 func main() {
 	cli := humacli.New(func(hooks humacli.Hooks, options *Options) {
-		srv := http.Server{
+		logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+		server := http.Server{
 			Addr: fmt.Sprintf(":%d", options.Port),
 			Handler: router.New("My API", "1.0.0",
 				func(w http.ResponseWriter, r *http.Request) {},
 				metrics.WriteProcessMetrics,
+				slogWriter{logger.With("tag", "api")},
 				(&handler.Greeting{}).Register,
 				(&handler.Contacts{Store: new(store.ContactsInmem)}).Register,
 			),
 			ReadHeaderTimeout: 15 * time.Second,
 		}
 		hooks.OnStart(func() {
-			err := srv.ListenAndServe()
+			err := server.ListenAndServe()
 			if err != http.ErrServerClosed {
 				slog.Error("failed to listen and serve", "err", err)
 			} else {
@@ -43,11 +47,19 @@ func main() {
 		hooks.OnStop(func() {
 			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 			defer cancel()
-			err := srv.Shutdown(ctx)
+			err := server.Shutdown(ctx)
 			if err != nil {
 				slog.Warn("could not shutdown the server", "err", err)
 			}
 		})
 	})
 	cli.Run()
+}
+
+type slogWriter struct{ *slog.Logger }
+
+func (sw slogWriter) Write(p []byte) (int, error) {
+	p = bytes.TrimSpace(p)
+	sw.Log(context.Background(), slog.LevelInfo, string(p))
+	return len(p), nil
 }
--- router/router.go
+++ router/router.go
@@ -3,7 +3,6 @@ package router
 import (
 	"fmt"
 	"io"
-	"log/slog"
 	"net/http"
 	"time"
 
@@ -14,7 +13,13 @@ import (
 
 var buckets = metrics.ExponentialBuckets(1e-3, 5, 6)
 
-func New(title, version string, readiness http.HandlerFunc, writeMetrics func(io.Writer), opts ...func(huma.API)) http.Handler {
+func New(
+	title, version string,
+	readiness http.HandlerFunc,
+	writeMetrics func(io.Writer),
+	logWriter io.Writer,
+	opts ...func(huma.API),
+) http.Handler {
 	set := metrics.NewSet()
 	set.RegisterMetricsWriter(writeMetrics)
 
@@ -27,12 +32,12 @@ func New(title, version string, readiness http.HandlerFunc, writeMetrics func(io
 	api := huma.NewGroup(root, "/api")
 	api.UseMiddleware(
 		func(ctx huma.Context, next func(huma.Context)) {
-			op, start := ctx.Operation(), time.Now()
+			proto, op, start := ctx.Version().Proto, ctx.Operation(), time.Now()
 			next(ctx)
 			labels := fmt.Sprintf(`{method="%s",path="%s",status="%d"}`, op.Method, op.Path, ctx.Status())
 			set.GetOrCreatePrometheusHistogramExt(`http_request_duration_seconds`+labels, buckets).UpdateDuration(start)
 			set.GetOrCreateCounter(`http_requests_total` + labels).Inc()
-			slog.Info(fmt.Sprintln(op.Method, op.Path, "from", ctx.RemoteAddr(), ctx.Status(), time.Since(start)))
+			fmt.Fprintln(logWriter, proto, op.Method, op.Path, ctx.RemoteAddr(), ctx.Header("User-Agent"), ctx.Status(), time.Since(start))
 		},
 	)
 	for _, opt := range opts {
