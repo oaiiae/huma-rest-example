59a10ba8b8e37e5aeb9ec1a76755896fc6d4ba60
Romain Libaert <git@rlibaert.dev>
2025-08-25 01:13:20 +0200

    refacto: logger

--- logger/logger.go
+++ logger/logger.go
@@ -4,57 +4,63 @@ import (
 	"io"
 	"log/slog"
 	"os"
+	"strings"
 )
 
-// Options for the CLI.
 type Options struct {
-	Output string `doc:"path to logfile" default:"-"`
-	Level  string `doc:"minimum log level" default:"info"`
-	Format string `doc:"log format" default:"text"`
+	Level   string `doc:"minimum log level" default:""`
+	Logfile string `doc:"write logs to file" default:""`
+	Format  string `doc:"log format" default:"text"`
 }
 
 func New(options *Options) *slog.Logger {
 	var err error
 
+	var opts slog.HandlerOptions
+	switch strings.ToLower(options.Level) {
+	case "":
+		opts.Level = nil
+	case "debug":
+		opts.Level = slog.LevelDebug
+	case "info":
+		opts.Level = slog.LevelInfo
+	case "warn":
+		opts.Level = slog.LevelWarn
+	case "error":
+		opts.Level = slog.LevelError
+	default:
+		options.Level = ""
+		logger := New(options)
+		logger.Warn("could not parse logger level", "err", err)
+		return logger
+	}
+
 	var output io.Writer
-	switch options.Output {
-	case "-":
+	switch options.Logfile {
+	case "":
 		output = os.Stdout
 	case os.DevNull:
 		return slog.New(slog.DiscardHandler)
 	default:
-		output, err = os.Open(options.Output)
+		output, err = os.Open(options.Logfile)
 		if err != nil {
-			options.Output = "-"
+			options.Logfile = ""
 			logger := New(options)
-			logger.Warn("could not open file", "err", err)
+			logger.Warn("could not open logger output", "err", err)
 			return logger
 		}
 	}
 
-	var level slog.Level
-	err = level.UnmarshalText([]byte(options.Level))
-	if err != nil {
-		options.Level = "info"
-		logger := New(options)
-		logger.Warn("could not parse level", "err", err)
-		return logger
-	}
-
-	opts := &slog.HandlerOptions{
-		Level: level,
-	}
-
 	var handler slog.Handler
-	switch options.Format {
+	switch strings.ToLower(options.Format) {
 	case "json":
-		handler = slog.NewJSONHandler(output, opts)
+		handler = slog.NewJSONHandler(output, &opts)
 	case "text":
-		handler = slog.NewTextHandler(output, opts)
+		handler = slog.NewTextHandler(output, &opts)
 	default:
 		options.Format = "text"
 		logger := New(options)
-		logger.Warn("unknown format")
+		logger.Warn("could not parse logger format")
 		return logger
 	}
 
